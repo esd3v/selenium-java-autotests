49e0fca053b483de4ea0aad294f2de13623d21dc
ES <57869510+esd3v@users.noreply.github.com>
2023-06-02 17:36:20 +0700

    Update components

--- src/test/java/com/theinternet/component/alert/AlertComponent.java
+++ src/test/java/com/theinternet/component/alert/AlertComponent.java
@@ -1,14 +1,17 @@
 package com.theinternet.component.alert;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import com.common.BaseComponent;
+import com.common.SeleniumHelper;
 
 public class AlertComponent extends BaseComponent {
     private String containerClass = "flash";
     private String closeClass = "close";
+    private String successClass = "success";
     private String errorClass = "error";
 
     public AlertComponent(WebDriver driver) {
@@ -31,7 +34,22 @@ public class AlertComponent extends BaseComponent {
         return alert.getAttribute("class").contains(errorClass);
     }
 
+    public boolean isSuccess(WebElement alert) {
+        return alert.getAttribute("class").contains(successClass);
+    }
+
     public void clickClose(WebElement alert) {
         getCloseButton(alert).click();
+    }
+
+    public boolean clickAlertCloseAndWaitToDetach(WebElement alert) {
+        clickClose(alert);
+
+        try {
+            SeleniumHelper.waitForElementToDetach(driver, alert, 1);
+            return true;
+        } catch (TimeoutException e) {
+            return false;
+        }
     }
 }
